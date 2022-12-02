5c7ebe00ca73c0baaf2d745ef1b4e26d7f6de6bb
AlexandrKataev <dkeyman@mail.ru>
2022-12-02 21:37:56 +0300

    refactoring

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -14,24 +14,24 @@ export const Header: React.FC = () => {
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#ddf0ff');
       setTheme(2);
     } else if (theme === 2) {
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#0072c5');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#0066af');
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m2', '#4348b1');
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#d0dcf1');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#d0d5f1');
       setTheme(3);
     } else if (theme === 3) {
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#8be000');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#92ec01');
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m2', '#43b158');
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#e1f8dd');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#ebfdd3');
       setTheme(4);
     } else if (theme === 4) {
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#f1d900');
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m2', '#b17f43');
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#fff9cb');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--m2', '#c59326');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#fbffd7');
       setTheme(5);
     } else if (theme === 5) {
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#f59600');
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m2', '#b14343');
-      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#f1d0d0');
+      document.querySelector<HTMLElement>(':root')?.style.setProperty('--b', '#fcfada');
       setTheme(6);
     } else if (theme === 6) {
       document.querySelector<HTMLElement>(':root')?.style.setProperty('--m', '#a900c5');
