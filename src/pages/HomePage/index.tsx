d5e0934d43a521a8ccd8e533b74abe57a1cc0245
AlexandrKataev <dkeyman@mail.ru>
2022-12-02 06:56:51 +0300

    add search component

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import s from './HomePage.module.scss';
-import { ReactComponent as SearchIcon } from 'shared/ui/icons/search.svg';
 
 import { taskData, taskService } from 'shared/api/services/taskService';
 import { TaskItem } from 'entities/TaskItem';
+import { Search } from 'shared/ui/Search';
 
 export const HomePage: React.FC = () => {
   const [taskList, setTaskList] = useState<taskData[]>([]);
@@ -30,15 +30,8 @@ export const HomePage: React.FC = () => {
 
   return (
     <div className={s.container}>
-      <div className={s.search}>
-        <input
-          className={s.input}
-          placeholder="Поиск"
-          value={searchValue}
-          onChange={onChangeSearch}
-        />
-        <SearchIcon className={s.icon} />
-      </div>
+      <Search searchValue={searchValue} onChangeSearch={onChangeSearch} />
+
       {taskList.length
         ? taskList
             .filter((el) => el.title.toLowerCase().includes(searchValue.toLowerCase()))
