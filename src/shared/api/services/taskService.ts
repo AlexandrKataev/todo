7cefd27ce137ff8488ed2fbf0908b18b67443faa
AlexandrKataev <dkeyman@mail.ru>
2022-12-06 05:23:46 +0300

    search by title

--- src/shared/api/services/taskService.ts
+++ src/shared/api/services/taskService.ts
@@ -4,7 +4,7 @@ import { baseUrl } from '../config';
 
 export const taskService = {
   getTaskList: async (searchValue: string) => {
-    const response = await axios.get<ITask[]>(`${baseUrl}/tasks?search=${searchValue}`);
+    const response = await axios.get<ITask[]>(`${baseUrl}/tasks?title=${searchValue}`);
 
     return response.data;
   },
