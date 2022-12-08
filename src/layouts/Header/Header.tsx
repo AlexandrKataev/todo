1d8398394a94ae29452167164f914e32f68290c7
AlexandrKataev <dkeyman@mail.ru>
2022-12-08 06:25:51 +0300

    useTheme hook

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -5,26 +5,15 @@ import styles from './Header.module.scss';
 import { ReactComponent as ThemeIcon } from 'shared/ui/icons/theme.svg';
 import { PlusIcon } from 'shared/ui/icons/PlusIcon';
 
-const themes = {
-  dark: 'dark',
-  light: 'light',
-};
-
-const getTheme = () => {
-  const theme = `${window?.localStorage?.getItem('theme')}`;
-  if (Object.values(themes).includes(theme)) return theme;
-  return themes.light;
-};
+import { useTheme } from 'shared/hooks/useTheme';
+import { getTheme, themes } from 'shared/helpers/getTheme';
 
 export const Header: FC = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
   const [theme, setTheme] = useState(getTheme);
-  useEffect(() => {
-    document.documentElement.dataset.theme = theme;
-    localStorage.setItem('theme', theme);
-  }, [theme]);
+  useTheme(theme);
 
   const onCLickNewTask = () => {
     navigate('task/new');
@@ -37,9 +26,7 @@ export const Header: FC = () => {
         <PlusIcon onClick={onCLickNewTask} width={'18px'} fill={'var(--color-header-text)'} />
         <ThemeIcon
           className={styles.theme}
-          onClick={() => {
-            theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light);
-          }}
+          onClick={() => (theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light))}
         />
       </div>
     </div>
