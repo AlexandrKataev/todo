d0cd5f024978586cca348f35c7e21b95ae217c8e
AlexandrKataev <dkeyman@mail.ru>
2022-12-12 13:08:59 +0300

    axios instance + env

--- src/shared/api/config/index.ts
+++
@@ -1,1 +0,0 @@
-export const API_URL = 'https://6367d292d1d09a8fa61b5b19.mockapi.io';
--- src/shared/api/services/taskService.ts
+++ src/shared/api/services/taskService.ts
@@ -1,25 +1,22 @@
-import axios from 'axios';
 import { ITask } from 'shared/models/ITask';
-import { API_URL } from '../config';
-
-axios.defaults.baseURL = API_URL;
+import { instance } from '../config/axios';
 
 export const taskService = {
   getTaskList: async (searchValue: string) => {
-    const response = await axios.get<ITask[]>(`/tasks?title=${searchValue}`);
+    const response = await instance.get<ITask[]>(`/tasks?title=${searchValue}`);
     return response.data;
   },
   getTask: async (id: string) => {
-    const response = await axios.get<ITask>(`/tasks/${id}`);
+    const response = await instance.get<ITask>(`/tasks/${id}`);
     return response.data;
   },
   updateTask: async (task: ITask) => {
-    await axios.put(`/tasks/${task.id}`, task);
+    await instance.put(`/tasks/${task.id}`, task);
   },
   createTask: async (task: ITask) => {
-    await axios.post(`/tasks`, task);
+    await instance.post(`/tasks`, task);
   },
   deleteTask: async (id: string) => {
-    await axios.delete(`/tasks/${id}`);
+    await instance.delete(`/tasks/${id}`);
   },
 };
